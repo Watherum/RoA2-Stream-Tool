74008ab5bfa2ba2db1e42c2d56830e53d844090d
Watherum <watherum@gmail.com>
2026-04-29 01:33:18 -0400

    V12.5.1 Better Centering of Values
    The html files should have better centering of the score and long names

--- Stream Tool/Resources/Scripts/Executable.js
+++ Stream Tool/Resources/Scripts/Executable.js
@@ -151,7 +151,7 @@ function createWindow() {
 
         backgroundColor: "#383838",
 
-        title: "RoA Stream Tool v12.5 [developer build]", // will get overwitten by gui html title
+        title: "RoA Stream Tool v12.5.1 [developer build]", // will get overwitten by gui html title
         icon: path.join(nodePath, 'icon.png'),
 
         webPreferences: {
